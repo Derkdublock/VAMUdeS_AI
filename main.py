8b5f9da3c9d5495eb1c196e2416ad691707ba9b8
Derkdublock <simoebl5@gmail.com>
2021-10-19 21:34:22 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+
+import time
+import pandas as pd
+
+dataframe = pd.DataFrame()
+
+
+
+timeObject = time.time()
+time.sleep(5)
+timeObject = time.time() - timeObject
+
+
+
+print(timeObject)
+
+
+
+
